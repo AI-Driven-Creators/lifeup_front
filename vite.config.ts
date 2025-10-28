f4280c1a8a5b1adea43963be044f799aa0428995
server <server@vultr.com>
2025-10-28 15:26:22 +0000

    add config for ssl

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
+import fs from 'node:fs'
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    port: 443,
+    host: '0.0.0.0',
+    allowedHosts: ['lifeup-study.top', '.lifeup-study.top'],
+    https: {
+      key: fs.readFileSync('/root/lfup/ssl/lifeup.key'),
+      cert: fs.readFileSync('/root/lfup/ssl/fullchain.crt')
     }
   }
 })
